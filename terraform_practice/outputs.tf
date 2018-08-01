191ef7cde50e3d9f02fda8d4ee78cbdfc02e1dd1
Chuck Larrieu Casias <chuckinator0@yahoo.com>
2018-08-01 14:24:21 -0700

    added private ip to terraform output

--- terraform_practice/outputs.tf
+++ terraform_practice/outputs.tf
@@ -15,12 +15,22 @@ output "public_subnets" {
   value       = ["${module.vpc.public_subnets}"]
 }
 
-
-output "instance_ips" {
+# Output public IP addresses
+output "instance_public_ips" {
   value = [
   # 'name' + ' ' + 'ip address'
   "${aws_instance.kafka-test.tags.Name} ${aws_instance.kafka-test.public_ip}",
   "${aws_instance.chef-test.tags.Name} ${aws_instance.chef-test.public_ip}",
   "${aws_instance.chef-workstation.tags.Name} ${aws_instance.chef-workstation.public_ip}"
+  ]
+}
+
+# Output private IP addresses
+output "instance_private_ips" {
+  value = [
+  # 'name' + ' ' + 'ip address'
+  "${aws_instance.kafka-test.tags.Name} ${aws_instance.kafka-test.private_ip}",
+  "${aws_instance.chef-test.tags.Name} ${aws_instance.chef-test.private_ip}",
+  "${aws_instance.chef-workstation.tags.Name} ${aws_instance.chef-workstation.private_ip}"
   ]
 }
